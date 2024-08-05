a7562eaaa54bec25880a343ec9ab1451fd856a14
bazarbay <bazarbay.tulenov@gmail.com>
2024-08-05 17:42:35 +0500

    adasdasd

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Exit the script as soon as a command fails
+set -e
+
+# Create mysql databases if none exists
+php artisan mysql:createdb
+
+# Run migrations
+php artisan migrate --force
+
+# Run migrations for sandbox too
+php artisan sandbox:migrate --force
+
+# Seed database
+php artisan fleetbase:seed
+
+# Restart queue
+php artisan queue:restart
+
+# Sync scheduler
+php artisan schedule-monitor:sync
+
+# Clear cache
+php artisan cache:clear
+
+# Initialize registry
+php artisan registry:init
